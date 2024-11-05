2263ee93d69fa621e77129a022d37298c10dd7d4
Arun Official 743 <arunarivalagan774@gmail.com>
2024-11-05 06:30:42 +0530

    book the movie slot and parking detail in db

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,34 +1,16 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react-swc';
 
-
-// export default defineConfig({
-//   server: {
-     
-//     proxy: {
-//       '/api': {
-        
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// });
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Backend server
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        target: 'http://localhost:6000',
+        secure: false,
       },
     },
   },
-  
   plugins: [react()],
 });
