7dc8562e94839ce4d7ad38ea9619bab7d39b6499
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-05-13 20:54:03 +0530

    netlify frontend url to cors  for deploy

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://cinematic-popcorn.netlify.app',
+  'https://ciniparkmafia.netlify.app',
 ];
 
 app.use(cors({
