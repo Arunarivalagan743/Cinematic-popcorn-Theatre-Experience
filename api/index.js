7df93a1c0e67b896be41d3c84041cad0d3e35241
Arunarivalagan743 <arunarivalagan774@gmail.com>
2025-05-18 18:42:21 +0530

    deploy of api

--- api/index.js
+++ api/index.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://ciniparkmak.netlify.app/',
+  'https://cinematic-popcorn-theatre-experience.vercel.app'
 ];
 
 app.use(cors({
